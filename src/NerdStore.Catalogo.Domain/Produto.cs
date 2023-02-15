816ffe2912bd6a46e4760a3a10ee3cd19a457af3
Daniel Rosa <daniel.rosa@ndd.tech>
2023-02-15 11:14:06 -0300

    criando entidade Produto

---
+++ src/NerdStore.Catalogo.Domain/Produto.cs
@@ -0,0 +1,14 @@
+﻿using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class Produto : Entity
+    {
+
+    }
+
+    public class Categoria : Entity
+    {
+
+    }
+}
