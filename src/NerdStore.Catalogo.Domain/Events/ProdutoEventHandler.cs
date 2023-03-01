2193792d4192e42f3664e7c57299e719ffe33dd4
Daniel Rosa <daniel.rosa@ndd.tech>
2023-03-01 07:48:15 -0300

    handler, event

---
+++ src/NerdStore.Catalogo.Domain/Events/ProdutoEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+
+namespace NerdStore.Catalogo.Domain.Events
+{
+    internal class ProdutoEventHandler : INotificationHandler<ProdutoAbaixoEstoqueEvent>
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public ProdutoEventHandler(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public async Task Handle(ProdutoAbaixoEstoqueEvent mensagem, CancellationToken cancellationToken)
+        {
+            var produto = await _produtoRepository.ObterPorId(mensagem.AggregateId);
+
+            // Enviar email para adiquirir mais produtos.
+        }
+    }
+}
---
+++ src/NerdStore.Core/Communication/IMediatrHandler.cs
@@ -0,0 +1,8 @@
+﻿using NerdStore.Core.Messages;
+namespace NerdStore.Core.Communication
+{
+    public interface IMediatrHandler
+    {
+        Task PublicarEvento<T>(T evento) where T : Event;
+    }
+}
