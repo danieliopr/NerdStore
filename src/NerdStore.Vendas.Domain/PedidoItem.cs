d1ed98b1847a696d9a055b9b70a907aee61f806b
Daniel Rosa <daniel.rosa@ndd.tech>
2023-03-15 08:29:19 -0300

    adicionando classes a vendas

--- src/NerdStore.Vendas.Domain/PedidoItem.cs
+++ src/NerdStore.Vendas.Domain/PedidoItem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NerdStore.Core.DomainObjects;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,50 @@ using System.Threading.Tasks;
 
 namespace NerdStore.Vendas.Domain
 {
-    internal class PedidoItem : Entity
+    public class PedidoItem : Entity
     {
+        public Guid PedidoId { get; private set; }
+        public Guid ProdutoId { get; private set; }
+        public string ProdutoNome { get; private set; }
+        public int Quantidade { get; private set; }
+        public decimal ValorUnitario { get; private set; }
+
+        // EF Rel.
+        public Pedido Pedido { get; set; }
+
+        public PedidoItem(Guid produtoId, string produtoNome, int quantidade, decimal valorUnitario)
+        {
+            ProdutoId = produtoId;
+            ProdutoNome = produtoNome;
+            Quantidade = quantidade;
+            ValorUnitario = valorUnitario;
+        }
+
+        protected PedidoItem() { }
+
+        internal void AssociarPedido(Guid pedidoId)
+        {
+            PedidoId = pedidoId;
+        }
+
+        public decimal CalcularValor()
+        {
+            return Quantidade * ValorUnitario;
+        }
+
+        internal void AdicionarUnidades(int unidades)
+        {
+            Quantidade += unidades;
+        }
+
+        internal void AtualizarUnidades(int unidades)
+        {
+            Quantidade = unidades;
+        }
+
+        public override bool EhValido()
+        {
+            return true;
+        }
     }
 }
--- src/NerdStore.Vendas.Domain/Voucher.cs
+++ src/NerdStore.Vendas.Domain/Voucher.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NerdStore.Core.DomainObjects;
 
 namespace NerdStore.Vendas.Domain
 {
-    internal class Voucher : Entity
+    public class Voucher : Entity
     {
+        public string Codigo { get; private set; }
+        public decimal? Percentual { get; private set; }
+        public decimal? ValorDesconto { get; private set; }
+        public int Quantidade { get; private set; }
+        public TipoDescontoVoucher TipoDescontoVoucher { get; private set; }
+        public DateTime DataCriacao { get; private set; }
+        public DateTime? DataUtilizacao { get; private set; }
+        public DateTime DataValidade { get; private set; }
+        public bool Ativo { get; private set; }
+        public bool Utilizado { get; private set; }
+
+        // EF Rel.
+        public ICollection<Pedido> Pedidos { get; set; }
+
+        //internal ValidationResult ValidarSeAplicavel()
+        //{
+        //    return new VoucherAplicavelValidation().Validate(this);
+        //}
     }
 }
